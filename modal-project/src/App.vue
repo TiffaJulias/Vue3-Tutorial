bd2316fc626f39fdfb8adb6cf85a783f24ba838f
Tiffa <tiffa.julias3@gmail.com>
2025-08-21 15:11:24 +0800

    Added click event modifiers.

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal :header="header" :text="text" :theme="theme"/>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" :theme="theme" @close="toggleModal()"/>
+  </div>
+  <button @click.alt="toggleModal()">open modal (alt)</button>
   <input type="text" ref="name">
   <button @click="handleClick()">click me</button>
 </template>
@@ -17,7 +21,8 @@ export default {
       title: 'My First Vue App :)',
       header: 'Sign up for the Giveaway!',
       text: 'Grab your ninja swag for half price!',
-      theme: 'sale'
+      theme: 'sale',
+      showModal: false
     }
   },
   methods: {
@@ -25,6 +30,9 @@ export default {
       console.log(this.$refs.name)
       this.$refs.name.classList.add('active');
       this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
     }
   }
 }
