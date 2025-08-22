ed3793b09665eb08e661ee7fd0d7edabc9ef3f18
Tiffa <tiffa.julias3@gmail.com>
2025-08-22 10:39:14 +0800

    Added new Modal and removed unecessary index.html and app.js files.

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -2,7 +2,22 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" :theme="theme" @close="toggleModal()"/>
+    <Modal :header="header" :text="text" :theme="theme" @close="toggleModal()" @open-modal2="openSaleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <p>Testing slots</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal2 :category="category" @close="showModal2 = false">
+      <h5>Items available:</h5>
+      <template v-slot:list v-if="(category === 'Clothing')">
+          <span><img class="hood" src="./assets/Hoodie.jpg"/><p>Hoodie</p></span>
+          <span><img class ="sweats" src="./assets/Sweatshirt.jpg"/><p>Sweatshirt</p></span>
+      </template>
+    </Modal2>
   </div>
   <button @click.alt="toggleModal()">open modal (alt)</button>
   <input type="text" ref="name">
@@ -11,10 +26,11 @@
 
 <script>
 import Modal from './components/Modal.vue';
+import Modal2 from './components/Modal2.vue';
 export default {
   name: 'App',
   components: {
-    Modal
+    Modal, Modal2
   },
   data() {
     return {
@@ -22,7 +38,9 @@ export default {
       header: 'Sign up for the Giveaway!',
       text: 'Grab your ninja swag for half price!',
       theme: 'sale',
-      showModal: false
+      category: 'Clothing',
+      showModal: false,
+      showModal2: false
     }
   },
   methods: {
@@ -33,6 +51,11 @@ export default {
     },
     toggleModal() {
       this.showModal = !this.showModal;
+      this.showModal2 = false; 
+    },
+    openSaleModal() {
+      this.showModal = false; 
+      this.showModal2 = true;
     }
   }
 }
@@ -51,5 +74,15 @@ h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+img.sweats{
+  width: 150px;
+  height: 100px;
+  border-radius: 10px;
+}
+img.hood {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
 }
 </style>
