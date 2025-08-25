38ac9aed89a06855dddb05f8da5aca524cbb948b
Tiffa <tiffa.julias3@gmail.com>
2025-08-25 12:38:32 +0800

    Added a new Vue Vite Project with form input and validation using Pico CSS.

---
+++ vite-ts/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <h1>{{ message }}</h1> 
+    <TaskForm @add-task="addATask"/>
+    <h3>There are {{ tasks.length }} tasks.</h3>
+  </main>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import TaskForm from "./components/TaskForm.vue"
+import type { Task } from './types'
+
+const message = ref("Tasks App")
+const tasks = ref<Task[]>([])
+
+function addATask(newTask: string) {
+  tasks.value.push({
+    id: crypto.randomUUID(),
+    title: newTask,
+    done: false
+  })
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ vite-ts/src/components/TaskForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <form @submit.prevent="formSubmitted()">
+      <label>
+        New Task
+        <input v-model="newTask" name="newTask" :aria-invalid="!!error || undefined" @input="error = ''">
+        <small v-if="error" id="invalid-helper">
+          {{ error }}
+        </small>
+      </label>
+      <div class="button-container">
+        <button>Add</button>
+      </div>
+    </form>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const newTask = ref("")
+const emit = defineEmits<{
+  addTask: [newTask: string]
+}>()
+const error = ref("")
+
+function formSubmitted() {
+  if (newTask.value.trim()) {
+    emit("addTask", newTask.value.trim())
+    newTask.value = ""
+  } else {
+    error.value = "Task cannot be empty!"
+  }
+}
+
+</script>
+<style>
+.button-container {
+  display: flex;
+  justify-content: end;
+}
+button {
+  background-color: #42b983;
+  color: white;
+}
+label {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* keeps them left-aligned */
+  text-align: left;
+}
+#invalid-helper {
+  color: red;
+  font-size: 0.9rem;
+  margin-top: 4px; /* add spacing below input */
+}
+
+</style>
