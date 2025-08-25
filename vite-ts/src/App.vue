53ef4fcbe94bf0e87ffe57745f08435ea0c881e9
Tiffa <tiffa.julias3@gmail.com>
2025-08-25 14:36:23 +0800

    Renamed 'Results.vue' to 'TaskList.vue' and showed the list of tasks from there using article tag.

--- vite-ts/src/App.vue
+++ vite-ts/src/App.vue
@@ -2,13 +2,15 @@
   <main>
     <h1>{{ message }}</h1> 
     <TaskForm @add-task="addATask"/>
-    <h3>There are {{ tasks.length }} tasks.</h3>
+    <h3 style="margin:20px">There are {{ tasks.length }} {{ checkTaskLength() }}</h3>
+    <TaskList :tasks="tasks"/>
   </main>
 </template>
 
 <script setup lang="ts">
 import {ref} from 'vue'
 import TaskForm from "./components/TaskForm.vue"
+import TaskList from "./components/TaskList.vue"
 import type { Task } from './types'
 
 const message = ref("Tasks App")
@@ -22,9 +24,19 @@ function addATask(newTask: string) {
   })
 }
 
+function checkTaskLength() {
+  if (tasks.value.length === 1) {
+    return "task."
+  } else {
+    return "tasks."
+  }
+}
+
 </script>
 
-<style scoped>
-
+<style>
+.boxes {
+  text-align: left;
+}
 
 </style>
---
+++ vite-ts/src/components/TaskList.vue
@@ -0,0 +1,13 @@
+<template>
+  <article class ="boxes" v-for="task in props.tasks" :key="task.id">
+    {{  task.title }}
+  </article>
+</template>
+
+<script lang="ts" setup>
+import type { Task } from "../types"
+
+const props = defineProps<{
+  tasks: Task[]
+}>()
+</script>
